9da744efcce6571813bfda39b1b486b891051df6
Colins Neil <colins.neil.nimalaraj@etud.u-picardie.fr>
2023-11-09 21:49:15 +0100

    Ajout de l'import du script de la classe joueur. Ajout des méthodes et d'un constructeur pour le joueur. Ajout du fichier .gitignore.

--- game/entities/player.py
+++ game/entities/player.py
@@ -1,10 +1,29 @@
 class Player:
     """
-    test
+    This the player class where all associated attributes and methods are.
     """
-    def __init__(self, start_pos, height, width, color, speed):
+
+    def __init__(self, canvas, start_pos, width, height, color, speed):
+        """
+        canvas: The canvas where the player need to be added (Canvas)
+        start_pos: The position of player when the game start (Tuple)
+        width: The width of the player (Integer)
+        height: The height of the player (Integer)
+        color: The color of the player (String)
+        speed: The player's movement speed (Integer)
+        """
+
+        self.canvas = canvas
         self.start_pos = start_pos
         self.width = width
         self.height = height
         self.color = color
         self.speed = speed
+    
+        self.player_canva_object = canvas.create_rectangle(self.start_pos[0], self.start_pos[1], self.start_pos[0] + self.width, self.start_pos[1] + self.height, fill=self.color, outline='white')
+
+    def up(self, event):
+        self.canvas.move(self.player_canva_object, 0, -self.speed)
+
+    def down(self, event):
+        self.canvas.move(self.player_canva_object, 0, self.speed)
--- game/main.py
+++ game/main.py
@@ -1,3 +1,4 @@
+from entities import player
 from tkinter import *
 
 # Creation of the window
@@ -10,15 +11,24 @@ width, height = 1280, 720
 canvas = Canvas(window, width=width, height=height)
 canvas.pack(fill='both', expand=True)
 
-paddle = canvas.create_rectangle(0, 0, 30, 150, fill='blue', outline='white')
+# Creation of the player 1
+p1_width = 10
+p1_height = 100
+p1_start_pos = (0, 0)
+p1_color = 'blue'
+p1_speed = 10
+p1 = player.Player(canvas, p1_start_pos, p1_width, p1_height, p1_color, p1_speed)
+canvas.bind_all('<z>', p1.up)
+canvas.bind_all('<s>', p1.down)
 
-def haut(event):
-    canvas.move(paddle, 0, -10)
-
-def bas(event):
-    canvas.move(paddle, 0, 10)
-
-canvas.bind_all('<Up>', haut)
-canvas.bind_all('<Down>', bas)
+# Creation of the player 2
+p2_width = 10
+p2_height = 100
+p2_start_pos = (width - p2_width, height - p2_height)
+p2_color = 'red'
+p2_speed = 10
+p2 = player.Player(canvas, p2_start_pos, p2_width, p2_height, p2_color, p2_speed)
+canvas.bind_all('<Up>', p2.up)
+canvas.bind_all('<Down>', p2.down)
 
 window.mainloop()
