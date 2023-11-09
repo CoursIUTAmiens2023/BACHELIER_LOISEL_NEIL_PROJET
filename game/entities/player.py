fee2bf3898253418e1e775dd0856652ef9f5d5b2
Colins Neil <colins.neil.nimalaraj@etud.u-picardie.fr>
2023-11-09 19:14:30 +0100

    Correction d'une erreur de nommage

--- game/entities/player.py
+++ game/entities/player.py
@@ -3,9 +3,6 @@ class Player:
     test
     """
     def __init__(self, start_pos, height, width, color, speed):
-        """
-        @start_pos is the start position
-        """
         self.start_pos = start_pos
         self.width = width
         self.height = height
