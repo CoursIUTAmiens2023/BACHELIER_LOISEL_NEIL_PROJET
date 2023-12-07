ccdf4cf7ccdebbdc5a5292c051a6fd90086608ef
Colins Neil <colins.neil.nimalaraj@etud.u-picardie.fr>
2023-12-07 17:16:34 +0100

    Mouvement joueur

--- game/entities/player.py
+++ game/entities/player.py
@@ -1,29 +1,34 @@
+import pygame
+
 class Player:
     """
     This the player class where all associated attributes and methods are.
     """
 
-    def __init__(self, canvas, start_pos, width, height, color, speed):
+    def __init__(self, screen, pos, width, height, color, speed):
         """
-        canvas: The canvas where the player need to be added (Canvas)
-        start_pos: The position of player when the game start (Tuple)
+        window: 
+        pos: The position of player when the game start (Tuple)
         width: The width of the player (Integer)
         height: The height of the player (Integer)
         color: The color of the player (String)
         speed: The player's movement speed (Integer)
         """
 
-        self.canvas = canvas
-        self.start_pos = start_pos
+        self.screen = screen
+        self.pos = pos
         self.width = width
         self.height = height
         self.color = color
         self.speed = speed
+        # self.rect = pygame.Rect(pos[0] - width/2, pos[1] - height/2, pos[0] + width/2, pos[1] + height/2)
+        self.rect = pygame.Rect(0, 0, 0 + width/2, 0 + height/2)
     
-        self.player_canva_object = canvas.create_rectangle(self.start_pos[0], self.start_pos[1], self.start_pos[0] + self.width, self.start_pos[1] + self.height, fill=self.color, outline='white')
+    def show_player(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
 
-    def up(self, event):
-        self.canvas.move(self.player_canva_object, 0, -self.speed)
+    # def up(self, event):
+    #     self.rect.move(self.player_canva_object, 0, -self.speed)
 
-    def down(self, event):
-        self.canvas.move(self.player_canva_object, 0, self.speed)
+    # def down(self, event):
+    #     self.rect.move(self.player_canva_object, 0, self.speed)
--- game/main.py
+++ game/main.py
@@ -1,13 +1,32 @@
+from entities import player
 import pygame
 
 pygame.init()
 
 width, height = 1280, 720
 
-window = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Pong Game")
 
 in_game = True
 
+x, y = 10, 10
+
+p1 = player.Player(screen, (1, 1), 5, 2, (0, 255, 0), 50)
+
 while in_game:
-    pygame.display.update()
+    for event in pygame.event.get():
+        if (event.type == pygame.QUIT):
+            in_game = False
+
+        if (event.type == pygame.KEYDOWN):
+            if (event.key == pygame.K_UP):
+                print("up")
+                x += 1
+            if (event.key == pygame.K_DOWN):
+                print("down")
+                x -= 1
+    
+    p1.show_player()
+    
+    pygame.display.flip()
