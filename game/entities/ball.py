51427aca04305ac4d1690de48b1897eca255e869
Colins Neil <colins.neil.nimalaraj@etud.u-picardie.fr>
2023-12-14 19:27:32 +0100

    Fix du mouvement de la balle

--- game/entities/ball.py
+++ game/entities/ball.py
@@ -3,26 +3,27 @@ import random
 
 class Ball:
     def __init__(self, screen, x, y, width, height, color, speed):
-        direction = [-1, 1]
-
         self.screen = screen
         self.color = color
         self.speed = speed
-        self.direction_x = self.speed * random.choice(direction)
-        self.direction_y = self.speed * random.choice(direction)
+        direction = [-1, 1]
+        self.movement_x = self.speed * random.choice(direction)
+        self.movement_y = self.speed * random.choice(direction)
         self.rect = pygame.Rect(x, y, width, height)
 
     def move(self):
         if self.rect.top < 0:
-            self.change_direction(direction_y=1)
+            self.change_direction(change_x_direction=False, change_y_direction=True)
         elif self.screen.get_height() < self.rect.bottom:
-            self.change_direction(direction_y=-1)
+            self.change_direction(change_x_direction=False, change_y_direction=True)
 
-        self.rect.move_ip(self.speed * self.direction_x, self.speed * self.direction_y)
+        self.rect.move_ip(self.movement_x, self.movement_y)
     
-    def change_direction(self, direction_x=1, direction_y=1):
-        self.direction_x = direction_x
-        self.direction_y = direction_y
+    def change_direction(self, change_x_direction=False, change_y_direction=False):
+        if change_x_direction:
+            self.movement_x = self.movement_x * -1
+        elif change_y_direction:
+            self.movement_y = self.movement_y * -1
 
     def show(self):
         """
