f77217d365e8b2a961656fb44fa23ecbf99cb252
QuentinLoisel <quentin.loisel@etud.u-picardie.fr>
2023-12-14 18:50:49 +0100

    Mise à jour ball.py

--- game/entities/ball.py
+++ game/entities/ball.py
@@ -2,16 +2,27 @@ import pygame
 import random
 
 class Ball:
-    def __init__(self, screen, x, y, width, height, color,speed):
+    def __init__(self, screen, x, y, width, height, color, speed):
+        direction = [-1, 1]
+
         self.screen = screen
         self.color = color
         self.speed = speed
-        self.rect = pygame.Rect(x,y,width,height)
+        self.direction_x = self.speed * random.choice(direction)
+        self.direction_y = self.speed * random.choice(direction)
+        self.rect = pygame.Rect(x, y, width, height)
 
     def move(self):
-        movements = [-self.speed,self.speed]
-        if(self.rect.top > 0 and self.rect.bottom < self.screen.get_height()):
-            self.rect.move_ip(random.choice(movements),self.speed)
+        if self.rect.top < 0:
+            self.change_direction(direction_y=1)
+        elif self.screen.get_height() < self.rect.bottom:
+            self.change_direction(direction_y=-1)
+
+        self.rect.move_ip(self.speed * self.direction_x, self.speed * self.direction_y)
+    
+    def change_direction(self, direction_x=1, direction_y=1):
+        self.direction_x = direction_x
+        self.direction_y = direction_y
 
     def show(self):
         """
@@ -19,4 +30,3 @@ class Ball:
         """
         
         pygame.draw.ellipse(self.screen, self.color, self.rect)
-
